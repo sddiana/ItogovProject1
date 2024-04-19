776a8812ffdf97946b38ae793028a3633979b72a
Diana <113444574+sddiana@users.noreply.github.com>
2024-04-19 07:35:04 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1713486756,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/tiledbackground.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/coin-sheet0.png",
+		"images/coin-sheet1.png",
+		"images/coin-sheet2.png",
+		"images/sprite-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/evil-sheet0.png",
+		"images/tiledbackground3.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
